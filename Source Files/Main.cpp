f828fc12d6421d0c015c83307c69032288d8a720
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-09-17 10:51:10 -0600

    Update Main.cpp

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -1,19 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 using namespace sf;
 
+void handleInput(Player& player, float deltatime) {
+    float speed = 200.0f; //200 pixels per second
+    if (Keyboard::isKeyPressed(Keyboard::Left)) player.move(-1, 0);
+    if (Keyboard::isKeyPressed(Keyboard::Right)) player.move(1, 0);
+    if (Keyboard::isKeyPressed(Keyboard::Up)) player.move(0, -1);
+    if (Keyboard::isKeyPressed(Keyboard::Down)) player.move(0, 1);
+}
 
 int main() {
     RenderWindow window(VideoMode(800, 600), "PACMAN Game");
+    Player player;
+    Clock clock;
 
     while (window.isOpen()) {
+        float deltatime = clock.restart().asSeconds();
 
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
         }
+        handleInput(player, deltatime);
 
         window.clear();
+
+        player.draw(window);
 
         window.display();
     }
