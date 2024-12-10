d09dd2bd6db013dde9d1dae79f859ffc081f478b
Ewan <146360250+BirdsArentRea1@users.noreply.github.com>
2024-12-10 10:16:15 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,9 @@
 #include "level.h"
 using namespace std;
 
-void handleInput(Player& player, float deltatime) {
-float speed = 200;
-if (Keyboard::isKeyPressed(Keyboard::Left)) player.move(-1, 0);
-if (Keyboard::isKeyPressed(Keyboard::Right)) player.move(1, 0);
-if (Keyboard::isKeyPressed(Keyboard::Up)) player.move(0, -1);
-if (Keyboard::isKeyPressed(Keyboard::Down)) player.move(0, 1);
-}
-
-
 int main() {
-    RenderWindow window(VideoMode(600, 830), "PACMAN Game");
-    Player player;
+    RenderWindow window(VideoMode(760, 1100), "PACMAN");
+    Player player(365,725);
     Clock clock;
     Level level;
 
@@ -26,16 +17,13 @@ int main() {
             if (event.type == Event::Closed)
                 window.close();
         }
-        handleInput(player, deltatime);
+        player.handleInput();
+        player.update(deltatime, level);
 
         window.clear();
-
         level.draw(window);
-
         player.draw(window);
-
         window.display();
     }
 
-    return 0;
 }
